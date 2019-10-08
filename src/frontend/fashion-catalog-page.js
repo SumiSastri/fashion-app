2c28579052aad88700206f0c940adfd9599831ce
Your Name <sumisastri@gmail.com>
2019-10-08 14:54:40 +0100

    adds class based component and img jsx as test

--- src/frontend/fashion-catalog-page.js
+++ src/frontend/fashion-catalog-page.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FashionCatalogPage = () => {
-	return (
-		<div>
-			<p className="f4 tc">I will be a fashion catalog page when I grow up!</p>
-		</div>
-	);
-};
+class FashionCatalogPage extends Component {
+	render() {
+		return (
+			<div>
+				<p className="f4 tc">I will be a fashion catalog page when I grow up!</p>
+				<img src="I will be pictures when I grow up" alt="fashion-catalog-images" />
+			</div>
+		);
+	}
+}
 export default FashionCatalogPage;
