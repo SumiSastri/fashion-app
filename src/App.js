f102e7297d81a9549a78ac3bd58cec9f46d058f6
Your Name <sumisastri@gmail.com>
2019-10-08 14:35:41 +0100

    adds front-end catalog component test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import FashionCatalogPage from './frontend/fashion-catalog-page';
 
 function App() {
 	return (
 		<div className="App">
 			<h1 className="f2 tc">Hello-World - Welcome to Intelligent Fashion</h1>
+			<FashionCatalogPage />
 		</div>
 	);
 }
---
+++ src/frontend/fashion-catalog-page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FashionCatalogPage = () => {
+	return (
+		<div>
+			<p className="f4 tc">I will be a fashion catalog page when I grow up!</p>
+		</div>
+	);
+};
+export default FashionCatalogPage;
