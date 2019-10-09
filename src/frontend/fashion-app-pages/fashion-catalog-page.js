1544974d02ebf67c928ab370d14af12dfcccafc2
Your Name <sumisastri@gmail.com>
2019-10-09 13:58:30 +0100

    sets up destructuring

--- src/frontend/fashion-app-pages/fashion-catalog-page.js
+++ src/frontend/fashion-app-pages/fashion-catalog-page.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const FashionCatalogPage = (props) => {
+	const { image, description, price } = props;
 	return (
 		<div className="womens-garments-page">
-			<h3 className="f2 tc">Women's Garments</h3>
 			<div className="image-container">
 				<img
 					className="dib br pa2 ma2 grow bg-animate transition: background-color .10s ease-in-out tc ba bw2 shadow-6"
-					src={props.image}
+					src={image}
 					alt="grey-dress"
 				/>
-				<h4 className="f4 pa1">{props.description}</h4>
-				<h5 className="f5 pa1">{props.price}</h5>
+				<h4 className="f4 pa1">{description}</h4>
+				<h5 className="f5 pa1">{price}</h5>
 			</div>
 		</div>
 	);
