678b25a5255fd10fb1e45df6fee72cc6f48f16a0
Your Name <sumisastri@gmail.com>
2019-10-09 15:27:51 +0100

    adds search functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-// import FashionCatalogPage from './frontend/fashion-app-pages/fashion-catalog-page';
 import SearchBox from './frontend/common-input-components/search';
 import GarmentList from './frontend/fashion-app-pages/garment-list';
 import { garments } from './data-cuts/peek-data/fashion-catalog-data/data-transformations';
 
-function App() {
-	return (
-		<div className="App">
-			<h1 className="f2 tc">The Ultimate Fashion Catalog</h1>
-			<SearchBox />
-			<GarmentList garments={garments} />
-		</div>
-	);
-}
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			garments: garments,
+			searchfield: ''
+		};
+	}
 
+	onSearchChange = (event) => {
+		// console.log(event.target.value);
+		this.setState({ searchfield: event.target.value });
+	};
+
+	render() {
+		const filteredGarments = this.state.garments.filter((garments) => {
+			return garments.product_title.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
+		// console.log(filteredGarments);
+		return (
+			<div className="App">
+				<h1 className="f2 tc">The Ultimate Fashion Catalog</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<GarmentList garments={filteredGarments} />
+			</div>
+		);
+	}
+}
 export default App;
--- src/frontend/common-input-components/search.js
+++ src/frontend/common-input-components/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SearchBox = (properties) => {
+const SearchBox = ({ searchfield, searchChange }) => {
 	return (
 		<div className="tr">
 			<div className="bg-black mw center pa2 br2-ns ba b--black-10">
@@ -9,6 +9,7 @@ const SearchBox = (properties) => {
 					className="pa2 bw1 w5 f3 h3 br-pill bg-light-yellow "
 					type="search"
 					placeholder="search our catalog"
+					onChange={searchChange}
 				/>
 			</div>
 		</div>
