e254c10b59cb8307f5eeb14774abe9632347a27a
Your Name <sumisastri@gmail.com>
2019-10-09 15:12:06 +0100

    changes styling

--- src/frontend/common-input-components/search.js
+++ src/frontend/common-input-components/search.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const SearchBox = (properties) => {
 	return (
-		<div className="bg-black mw center pa2 br2-ns ba b--black-10">
-			<input
-				className="pa2 bw1 w5 f3 h3 br-pill bg-light-yellow "
-				type="search"
-				placeholder="search our catalog"
-			/>
+		<div className="tr">
+			<div className="bg-black mw center pa2 br2-ns ba b--black-10">
+				<h3 className="tc white f3">Women's Garments</h3>
+				<input
+					className="pa2 bw1 w5 f3 h3 br-pill bg-light-yellow "
+					type="search"
+					placeholder="search our catalog"
+				/>
+			</div>
 		</div>
 	);
 };
